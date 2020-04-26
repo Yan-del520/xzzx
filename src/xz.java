d96d4c61f86ed93aac8b4ffb64177bbd32aaa5b3
Yan-del520 <yan0409@126.com>
2020-04-26 18:13:23 +0800

    Merge branch 'master' of D:\ideaProjects\xzzx with conflicts.

--- src/xz.java
+++ src/xz.java
@@ -1,5 +1,7 @@
 public class xz {
     public static void main(String[] args) {
         System.out.println("宝莲灯沉香");2langs
+        System.out.println("宝莲灯沉香");
+        System.out.println("他舅二郎神");
     }
 }
